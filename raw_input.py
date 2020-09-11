12721fbdd59644baaf74c1d8df30ddeee9652750
Brian Glassman <>
2020-09-11 15:50:16 -0400

    move changes from simple_piecemeal into main files

--- raw_input.py
+++ raw_input.py
@@ -8,19 +8,22 @@ Created on Sat Aug  1 16:32:23 2020
 #%%
 Parses raw input files from banks, Venmo, etc.
 
-TODO
-    should parsers be functions or classes?
-        Classes make it easier to group information and helper methods
+Parsers are classes to make it easier to group information and helper methods
 """
 
-#%%
+#%% RawTransaction definition
 # TODO move to library
 import datetime
 import dateutil
 from dataclasses import dataclass
+import re # Needed for isinstance check
+
+from templates import templates
+
 @dataclass
 class RawTransaction:
     """Transactions as read in from raw input files."""
+
     date: datetime.date
     value: float
     desc: str
@@ -34,27 +37,174 @@ class RawTransaction:
         self.auto_cat = auto_cat
         self.account = account
 
-    def __post_init__(self):
-        """Format fields correctly.
-
-        Use dataclass post_init rather than redefining __init__ entirely
-        to keep using the auto-generated repr and other methods"""
+    def __make_date(self, raw):
+        """Convert input to the appropriate type"""
 
         # Ensure date is a consistent type to avoid many, many headaches
-        assert isinstance(self.date, datetime.date), \
-            f"Date field must be a datetime.date instance, got {type(self.date)}"
+        # Can't use isinstance, because datetime.datetime is a subclass of datetime.date
+        if type(raw) is datetime.date:
+            return raw
+        elif isinstance(raw, datetime.date):
+            return raw.date()
+        else:
+            return dateutil.parser.parse(raw).date()
+
+    def to_dict(self):
+        """Return a dictionary of the contained values"""
+        return self.__dict__
+
+    # Helper functions to check different fields
+    def __check_desc(self, pattern):
+        """Assumes that pattern contains a desc field"""
+        mask = pattern['desc']
+        desc = self.desc
+        if isinstance(mask, re.Pattern):
+            # Regex matching (None if no match found)
+            return mask.search(desc) is not None
+        elif isinstance(mask, str):
+            # Exact string matching
+            return desc == mask
+        else:
+            raise NotImplementedError("Unknown mask type")
+    def __check_value(self, pattern):
+        """Assumes that pattern contains a value field"""
+        mask = pattern['value']
+        if isinstance(mask, (int, float)):
+            # Exact value matching
+            return self.value == mask
+        else:
+            # Range matching
+            assert len(mask) == 2
+            assert mask[0] < mask[1]
+            return mask[0] <= self.value <= mask[1]
+
+    # Mapping from field name to helper function
+    _checker = {'desc': __check_desc,
+                'value': __check_value,
+                }
+
+    def match_templates(self):
+        """Check self against the common templates. Return whichever template
+        matches, or None if no match"""
+
+        matched = None
+        # Check the transaction against all templates in order
+        for template in templates:
+            old = template['old']
+            match = True
+            for key in old:
+                # Run the checker for each field that has a pattern
+                match = match and self._checker[key](self, old)
+                if not match: break # Exit loop if any pattern fails
+
+            if match:
+                # Template matched, stop searching
+                matched = template
+                break
+        return matched
+
+    def apply_template(self, template):
+        """Convert self from RawTransaction to one or more Transactions as
+        specified by the template"""
+
+        new = template['new']
+        if isinstance(new, dict):
+            # One-to-one conversion, make a list for iterating
+            new = [new]
+        else:
+            # Assume one-to-many conversion, already a list
+            pass
+
+        ret = []
+        for n in new:
+            # TODO use alternate constructor once implemented
+            #   Or some other smart way of converting RawTransaction to Transaction
+            # Deep copy n to avoid changing the base common templates
+            n = dict(n)
+            n['recurrence'] = n.pop('split')
+
+            d = self.to_dict()
+            d.update(n)
+            # Keep only keys that are used for Transaction
+            d = {k:v for k,v in d.items() if k in Transaction.fields()}
+            ret.append(Transaction(**d))
+        return ret
+
+def test_checker():
+    #TODO test splitting transaction
+    #TODO test trivial cases
+    #TODO test value range matching
+    return True
+
+def test_RawTransaction():
+    # TODO
+    # date as string
+    # date as datetime.datetime
+    # date as datetime.date
+    # empty description
+	# value as int
+	# value as float
+	# value as negative int
+	# value as negative float
+    # value as string like "##.##" <-- normal case float
+    # value as string like "##" <-- normal case int
+    # value as string like "-##.####" <-- normal case negative float
+    # value as string like "-##" <-- normal case negative int
+    # value as string like "$##.##"
+    # should NOT test any weirder values. That's the parser's job to take care of
+    return True
+
+#%% Transaction definition (TO BE MOVED ELSEWHERE)
+# TODO move to library
+import datetime
+import dataclasses
+from dataclasses import dataclass
+@dataclass
+class Transaction:
+    """Transactions as used internally (has been split, categorized, etc.)"""
+
+    date: datetime.date
+    value: float
+    desc: str
+    category: str
+    recurrence: str
+    # TODO add raw_id and mod_id
+
+    # TODO alternate constructor for creating Transaction out of RawTransaction
+
+    def __init__(self, date, value, desc=None, category=None, recurrence=None):
+        self.date = self.__make_date(date)
+        self.value = float(value)
+        self.desc = desc if desc is not None else ""
+        self.category = category
+        self.recurrence = recurrence
 
     def __make_date(self, raw):
         """Convert input to the appropriate type"""
-        return dateutil.parser.parse(raw).date()
 
-#%%
+        # Ensure date is a consistent type to avoid many, many headaches
+        # Can't use isinstance, because datetime.datetime is a subclass of datetime.date
+        if type(raw) is datetime.date:
+            return raw
+        elif isinstance(raw, datetime.date):
+            return raw.date()
+        else:
+            return dateutil.parser.parse(raw).date()
+
+    @staticmethod
+    def fields():
+        """Return a list of field names as strings"""
+        return [x.name for x in dataclasses.fields(Transaction)]
+
+#%% Definitions
 import csv
 from abc import ABC
+
 
 def parse(infile, parser, *args, **kwargs):
     """Run the provided parsing logic on the given file."""
     return parser.parse(infile, *args, **kwargs)
+
 
 class USAA_parser(ABC):
     """Namespace for handling csv output files from USAA.
@@ -69,7 +219,7 @@ class USAA_parser(ABC):
 
     @classmethod
     def parse(cls, infile, account):
-        """Parse the line.
+        """Parse the given file, tagging transactions as the given account.
 
         Args:
             infile (file path): path to the input file to open
@@ -89,6 +239,8 @@ class USAA_parser(ABC):
         Returns:
             raw_transaction object
         """
+        # Make empty fields None
+        line = {k:(v if v else None) for k,v in line.items()}
         desc = cls.get_desc(**line)
         value = cls.format_value(**line)
         return RawTransaction(date = line['date'],
@@ -148,5 +300,166 @@ def venmo_parser():
 
     """
 
+
 def cash_parser():
     pass
+
+
+class excel_parser(ABC):
+    """Namespace for parsing my existing Excel files"""
+
+    xlfile = 'ref_Budget Buckets.xlsm'
+
+    fieldnames = {'Date': 'date',
+                  'Custom Description': 'custom_desc',
+                  'Auto Description': 'auto_desc',
+                  'USAA Category': 'auto_cat',
+                  'Raw Amount': 'value',
+                  'Account': 'account',}
+
+    @classmethod
+    def parse(cls, infile=xlfile, accounts=None):
+        """Parse the file.
+
+        Args:
+            infile (file path): path to the input file to open
+        """
+        import openpyxl
+        wb = openpyxl.load_workbook(filename=cls.xlfile, read_only=False, data_only=True)
+
+        out = []
+        try:
+            # Different years are stored in different sheets
+            sheets = ['Log 2018']
+            for ws in (wb[sheetname] for sheetname in sheets):
+                # Each row after the header is one transaction
+                # Skip header in row 1
+
+                # Format as {field : 0-indexed column number}, only keep used columns
+                header = {cell.value:c for c,cell in enumerate(ws[1]) if cell.value in cls.fieldnames.keys()}
+                # no min_col because that would mess up the indexing
+                max_col = max(header.values()) + 1  # max_col parameter uses 1-index
+
+                for raw_row in ws.iter_rows(min_row=2, max_col=max_col, values_only=True):
+                    row = {cls.fieldnames[field]:raw_row[c] for field,c in header.items()}
+
+                    # Filter by account
+                    if accounts is not None and row['account'] in accounts:
+                        out.append(cls.make_transaction(row))
+        finally:
+            # Openpyxl doesn't release open read-only workbooks
+            wb.close()
+
+        return out
+
+
+    @classmethod
+    def make_transaction(cls, line):
+        """Convert a line from the input file into a RawTransaction object.
+
+        Args:
+            line (dict of strings): the line read in from the input file
+        Returns:
+            raw_transaction object
+        """
+        desc = cls.get_desc(**line)
+        value = cls.format_value(**line)
+        return RawTransaction(date = line['date'],
+                              desc = desc,
+                              auto_cat = line['auto_cat'],
+                              value = value,
+                              account = line['account'])
+
+    @staticmethod
+    def get_desc(custom_desc, auto_desc, **excess):
+        """Combines custom_desc and auto_desc
+        Args:
+            custom_desc (str): manually input description
+            auto_desc (str): automatically generated description
+            **excess: unused fields from the input line
+        Returns:
+            one desc filled: return unchanged
+            both filled: "[auto] --- [custom]"
+            neither: empty string
+        """
+        if custom_desc and auto_desc:
+            return f"{auto_desc} --- {custom_desc}"
+        elif custom_desc and (not auto_desc): return custom_desc
+        elif (not custom_desc) and auto_desc: return auto_desc
+        elif (not custom_desc) and (not auto_desc): return ""
+        else: raise RuntimeError  # Shouldn't be possible
+
+    @staticmethod
+    def format_value(value, **excess):
+        """Properly format the value
+        Args:
+            value (str): monetary value. Positive values may have "--" prefix
+            **excess: unused fields from the input line
+        Returns:
+            value as a float
+        """
+        if isinstance(value, str) and value.startswith('--'):
+            return float(value[2:])
+        else:
+            return float(value)
+
+class excel_cat_parser(excel_parser):
+    fieldnames = {'Date': 'date',
+                  'Custom Description': 'custom_desc',
+                  'Auto Description': 'auto_desc',
+                  'Raw Amount': 'value',
+                  'Account': 'account',
+                  'My Category': 'category',
+                  'Split_raw': 'split'}
+
+    @staticmethod
+    def get_recurrence(split, **excess):
+        # Apply recurrence
+        if split == 1:
+            # Single transaction
+            return None
+        else:
+            # Recurring transaction
+            # Have to do some fuzzy logic, this won't be in production
+            # NOTE: initial count is reduced by one to account for first instance
+            if split == 14:
+                # Salary
+                return '14d' # Apply over 14 days, don't normalize
+            elif 28 <= split <= 31:
+                # Monthly
+                return '1m' # Apply over 1 month, don't normalize
+            elif 365 <= split <= 366:
+                # Yearly
+                return '1y' # Apply over 1 year, don't normalize
+
+    @classmethod
+    def make_transaction(cls, line):
+        """Convert a line from the input file into a Transaction object.
+
+        Args:
+            line (dict of strings): the line read in from the input file
+        Returns:
+            raw_transaction object
+        """
+        desc = cls.get_desc(**line)
+        value = cls.format_value(**line)
+        rec = cls.get_recurrence(**line)
+        return Transaction(date = line['date'],
+                           desc = desc,
+                           category = line['category'],
+                           value = value,
+                           recurrence = rec)
+
+#%% Testing
+def test_USAA_parser():
+    log = """posted,,12/31/2018,,DGX - PHILADELPHIA       PHILADELPHIA PA,General Merchandise,-9.35
+             posted,,5/1/2018,,USAA CREDIT CARD PAYMENT SAN ANTONIO  TX,Credit Card Payments,--507.83
+             posted,,2/6/2018,,AVE C MKT xxxxxx0062     TROY         MI,Restaurants/Dining,-1.79"""
+    tempfile = 'test_USAA_parser.csv'
+    assert True == True
+
+    # TODO check out pytest fixtures, see if it can be used to make temporary files
+
+
+if __name__ == "__main__":
+    test_USAA_parser()
--- simple_piecemeal/add_new.py
+++ simple_piecemeal/add_new.py
@@ -18,7 +18,7 @@ DOES NOT AND WILL NOT:
 
 import re
 
-import raw_input_pm as raw_input
+import raw_input
 
 #%% Read in raw transactions
 raw_ = []
@@ -33,162 +33,16 @@ raw_.extend(raw_input.parse(r"D:\Users\Brian\Downloads\chk.csv",
 raw_.pop(21)
 raw_[20].value = -1690
 
-#%% Define common templates
-#TODO replace with named tuple or custom class so keys aren't just strings
-#TODO save off as a file (JSON needs lots of formatting and custom re parser)
-
-# Regex notes:
-#   \*{11} matches exactly 11 asterisks
-common = [# Utilities / Rent
-          {#//Phone
-           'old': {'desc':'SPRINT *WIRELESS         800-639-6111 KS'},
-           'new': {'category': 'Utilities', 'split': 30}},
-          {#//PECO
-           'old': {'desc': re.compile('^PECOENERGY       UTIL_BIL   \*{11}')},
-           'new': {'category': 'Utilities', 'split': 30}},
-          {#//PGW
-           'old': {'desc': re.compile('^PGW WEBPAY       UTILITY    \*{11}')},
-           'new': {'category': 'Utilities', 'split': 30}},
-          {#//Aritom water bill
-           'old': {'desc': 'ARITOM PROPERTIES        610-353-4925 PA',
-                   'value': -40},
-           'new': {'category': 'Utilities', 'split': 30}},
-          {#//Aritom rent
-           'old': {'desc': 'ARITOM PROPERTIES        610-353-4925 PA',
-                   'value': -1650},
-           'new': {'category': 'Rent', 'split': 30}},
-          {#//Aritom rent and water bill
-           'old': {'desc': 'ARITOM PROPERTIES        610-353-4925 PA',
-                   'value': -1690},
-           'new': [{'value': -1650, 'category': 'Rent', 'split': 30},
-                   {'value': -40, 'category': 'Utilities', 'split': 30}]},
-
-          # Banking
-          {#//CC Payments
-           'old': {'desc': re.compile('^USAA CREDIT CARD PAYMENT*')},
-           'new': {'category': 'CC Payments', 'split': 1}},
-          {#//Cash rewards credit
-           'old': {'desc': 'CASH REWARDS CREDIT',
-                   'value': (0, 20)},
-           'new': {'category': 'Income - Other', 'split': 1}},
-          {#//Interest
-           'old': {'desc': 'INTEREST PAID',
-                   'value': (0, 5)},
-           'new': {'category': 'Income - Other', 'split': 1}},
-
-          # Other
-          {#//Grubhub
-           'old': {'desc': re.compile('^PAYPAL \*GRUBHUBFOOD')},
-           'new': {'category': 'Food - nice', 'split': 1}},
-          {#//Salary
-           'old': {'desc': re.compile('^AGUSTAWESTLAND P DIRECT DEP \*{11}'),
-                   # TODO add entries for other pay rates, with dates
-                   'value': (1902.79, 1902.81)},
-           'new': {'category': 'Salary', 'split': 14}},
-          {#//Spotify
-           'old': {'desc': 'PAYPAL           INST XFER  ***********USAI',
-                   'value': -10.81},
-           'new': {'desc': 'Spotify',
-                   'category': 'Entertainment - Other', 'split': 30}},
-          {#//Car note
-           'old': {'desc': re.compile('^HONDA PMT        8004489307 \*{11}'),
-                   'value': -320},
-           'new': {'category': 'Car - Note', 'split': 30}},
-          {#//Insurance
-           'old': {'desc': 'USAA P&amp;C INT     AUTOPAY    ***********1608'},
-           'new': {'category': 'Car/Rental Insurance', 'split': 30}},
-          ]
-
-# Helper functions to check different fields
-def _check_desc(transaction, template):
-    """Assumes that template contains a desc field"""
-    mask = template['desc']
-    desc = transaction.desc
-    if isinstance(mask, re.Pattern):
-        # Regex matching (None if no match found)
-        return mask.search(desc) is not None
-    elif isinstance(mask, str):
-        # Exact string matching
-        return desc == mask
-    else:
-        raise NotImplementedError("Unknown mask type")
-def _check_value(transaction, template):
-    """Assumes that template contains a value field"""
-    mask = template['value']
-    val = transaction.value
-    if isinstance(mask, (int, float)):
-        # Exact value matching
-        return val == mask
-    else:
-        # Range matching
-        assert len(mask) == 2
-        assert mask[0] < mask[1]
-        return mask[0] <= val <= mask[1]
-_checker = {'desc': _check_desc,
-            'value': _check_value,
-            }
-
-# Main function to check RawTransaction against a template
-def template_check(raw):
-    #TODO more automaticish, given old/new are separate dicts
-    assert isinstance(raw, raw_input.RawTransaction)
-    matched = None
-    # Check the transaction against all templates in order
-    for template in common:
-        old = template['old']
-        new = template['new']
-        match = True
-        for key in old:
-            # Run the checker for each field that has a pattern
-            match = match and _checker[key](raw, old)
-            if not match: break # Exit loop if any pattern fails
-
-        if match:
-            # Template matched, stop searching
-            matched = new
-            break
-    if matched is None:
-        # No template matched
-        return None
-    else:
-        # Template matched, apply it
-
-        if isinstance(new, dict):
-            # One-to-one conversion, make a list for iterating
-            new = [new]
-        else:
-            # Assume one-to-many conversion, already a list
-            pass
-
-        ret = []
-        for n in new:
-            # TODO use alternate constructor once implemented
-            #   Or some other smart way of converting RawTransaction to Transaction
-            # Deep copy n to avoid changing the base common templates
-            n = dict(n)
-            n['recurrence'] = n.pop('split')
-
-            d = raw.to_dict()
-            d.update(n)
-            # Keep only keys that are used for Transaction
-            d = {k:v for k,v in d.items() if k in raw_input.Transaction.fields()}
-            ret.append(raw_input.Transaction(**d))
-        return ret
-
-#TODO test splitting transaction
-#TODO test trivial cases
-#TODO test value range matching
-
 #%% Check transactions against templates
 found = []
 not_found = []
 for raw in raw_:
-    trans = template_check(raw)
-    if trans is None:
+    template = raw.match_templates()
+    if template is None:
         # No template matched
         not_found.append(raw)
     else:
         # Template matched, Transaction created
-        found.extend(trans)
+        found.extend(raw.apply_template(template))
 
 print('\n'.join(str(x) for x in not_found))
