dcaf86cc002dba259f93262be5c90dc08049060f
Brian Glassman <15279030+BrianGlassman@users.noreply.github.com>
2023-01-01 16:21:46 -0700

    delete old files for January 2023 rework

--- database/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Database package
-Handles all the reading and writing from the various databases
-    Ex.
-
-Other scripts should import from the package for the sake of abstraction/encapsulation
-"""
