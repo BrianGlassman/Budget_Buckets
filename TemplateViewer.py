88b4adb84c8307a3e5499d109d02979646eddc89
Brian Glassman <>
2023-02-14 10:26:40 -0700

    display the selected Template (lazily)

--- TemplateViewer.py
+++ TemplateViewer.py
@@ -12,6 +12,8 @@ root = gui.Root(10, 10, "Templates")
 
 bd = 10
 
+bold = ('', 0, 'bold')
+
 # Create everything first
 outer_frame = gui.Frame(root, name='outer_frame',
     bg='black', bd=bd, relief='ridge',)
@@ -24,16 +26,60 @@ detail_frame = gui.Frame(root, name='detail_frame',
 # Connect the scrollbar to things
 tree.configure(xscrollcommand=hscroll.set, yscrollcommand=vscroll.set)
 
-# Fill the tree
+leaf_map = {}
+# Fill the tree (nested version)
+# for trunk_name, trunk in nest_templates.items():
+#     trunk_id = tree.insert(parent='', index='end', text=trunk_name, tags='trunk')
+#     for branch_name, branch in trunk.items():
+#         branch_id = tree.insert(parent=trunk_id, index='end', text=branch_name, tags='branch')
+#         for leaf in branch:
+#             assert isinstance(leaf, Categorize.Template)
+#             leaf_id = tree.insert(parent=branch_id, index='end', text=leaf.name, tags='leaf')
+#             leaf_map[leaf_id] = leaf
+# Fill the tree (flat version)
 for trunk_name, trunk in nest_templates.items():
     trunk_id = tree.insert(parent='', index='end', text=trunk_name, tags='trunk')
     for branch_name, branch in trunk.items():
-        branch_id = tree.insert(parent=trunk_id, index='end', text=branch_name, tags='branch')
+        branch_id = tree.insert(parent='', index='end', text=branch_name, tags='branch')
         for leaf in branch:
             assert isinstance(leaf, Categorize.Template)
-            tree.insert(parent=branch_id, index='end', text=leaf.name, tags='leaf')
-tree.tag_configure('trunk', font=('', 0, 'bold'))
-tree.tag_configure('branch', font=('', 0, 'bold'))
+            leaf_id = tree.insert(parent='', index='end', text=leaf.name, tags='leaf')
+            leaf_map[leaf_id] = leaf
+# Make non-leaves bold
+tree.tag_configure('trunk', font=bold)
+tree.tag_configure('branch', font=bold)
+
+# Tree callback
+def on_select(_):
+    selection = tree.selection()
+
+    # Only care about when single Templates (leaves) are selected
+    if len(selection) != 1: return placeholder_detail()
+    id, = selection
+    tree_item = tree.item(id)
+    if 'leaf' not in tree_item['tags']: return placeholder_detail()
+
+    t = leaf_map[id]
+    return make_detail(t)
+tree.bind('<<TreeviewSelect>>', on_select)
+
+# Detail pane
+def placeholder_detail():
+    frame = detail_frame
+    frame.clear()
+    label = gui.tkinter.Label(frame, text='Select a Template to view', font=bold)
+    label.pack(side='top', anchor='center', fill='both', expand=True)
+def make_detail(t: Categorize.Template):
+    frame = detail_frame
+    frame.clear()
+    name = gui.tkinter.Label(frame, text=t.name, font=bold)
+    name.pack(side='top', anchor='nw', fill='x', expand=False)
+    for k,v in t.as_dict().items():
+        if k == name: continue
+        label = gui.tkinter.Label(frame, text=k.capitalize(), anchor='w', font=bold)
+        label.pack(side='top', anchor='w')
+        value = gui.tkinter.Label(frame, text=str(v), anchor='w')
+        value.pack(side='top', anchor='w', fill='x', expand=False)
 
 # Pack everything
 outer_frame.pack(side='left', fill='y', expand=False)
@@ -42,5 +88,8 @@ hscroll.pack(side='bottom', fill='x', expand=False)
 tree.pack(side='left', fill='both', expand=True)
 tree.column('#0', stretch=False)
 detail_frame.pack(side='left', fill='both', expand=True)
+
+# Initialize detail frame
+placeholder_detail()
 
 root.mainloop()
