43dedccc7c0f32b24536b8ccc03295288f7791ed
Brian Glassman <>
2023-02-17 18:35:52 -0700

    don't hard-code bucket info

--- Root/Buckets.py
+++ Root/Buckets.py
@@ -1,3 +1,10 @@
+# Path shenanigans for relative import when running directly
+if __name__ == "__main__":
+    import os, sys
+    path = os.path.dirname(__file__)
+    path = os.path.dirname(path)
+    sys.path.append(path)
+
 from Root import Constants as _import_Constants
 
 class Bucket:
@@ -6,54 +13,22 @@ class Bucket:
         self.max_value = max_value
         self.refill = monthly_refill / 30
 #%% Define buckets
-_bucket_info: dict[_import_Constants.CatType, tuple]
-_bucket_info = { # {category: (max, monthly)}
-    # Car
-    'Car - Note': (320, 320),
-    'Car/Rental Insurance': (175, 160),
-    'Car - Other': (300, 75),
-    'Car - Parking Pass': (75, 50), # TODO change to just "Parking" or split pass from incidental. Can't do until after porting over old sheet
+def _read_buckets() -> dict[_import_Constants.CatType, Bucket]:
+    with open(_import_Constants.BucketInfo_file, 'r', newline='') as f:
+        lines = [line.strip() for line in f.readlines()]
+    lines = [line for line in lines if line] # Remove empty lines
+    lines = [line for line in lines if not line.startswith('#')] # Remove comment lines
 
-    # Education
-    'Self-improvement': (100, 10),
+    # Add the to-do category
+    lines.append(f'{_import_Constants.todo_category},0,0')
 
-    # Entertainment
-    'Dates': (150, 50),
-    'Entertainment - Other': (75, 20),
-    'Games': (75, 20),
-    'Going Out': (150, 50),
-    'Books': (75, 20),
-    'Big Fun': (500, 20),
-
-    # Food
-    'Groceries': (300, 250),
-    'Food - nice': (100, 80),
-
-    # Housing
-    'Rent': (1850, 1850),
-    'Utilities': (200, 150),
-    'Internet': (40.41, 40.41),
-    'Housing - Other': (150, 100),
-    'Decoration': (150, 20),
-
-    # Investments/Savings
-    '401k': (0, 0),
-    'Retirement': (0, 0),
-    'Long-term': (0, 0),
-    'Unexpected Fund': (0, 0),
-
-    # Medical/Dental
-    'Medical - Other': (200, 20),
-    'Medical Insurance': (0, 0),
-
-    # Other
-    'ATM': (200, 25),
-    'Other - Other': (200, 20),
-
-    # Personal Care / Clothing
-    'Clothes/Personal care': (100, 30),
-
-    # Uncategorized
-    _import_Constants.todo_category: (0, 0),
-}
-bucket_info = {category:Bucket(category, max_value, monthly_refill) for category, (max_value, monthly_refill) in _bucket_info.items()}
+    bucket_info = {}
+    for line in lines[1:]:
+        category, max_s, monthly_s = line.split(',')
+        max_f = float(max_s)
+        monthly_f = float(monthly_s)
+        bucket = Bucket(name=category, max_value=max_f, monthly_refill=monthly_f)
+        assert category not in bucket_info
+        bucket_info[category] = bucket
+    return bucket_info
+bucket_info = _read_buckets()
--- Root/Constants.py
+++ Root/Constants.py
@@ -12,12 +12,14 @@ Templates_file = _import_os.path.join('Categorize', 'Templates.json')
 ManualAccountHandling_file = _import_os.path.join('Categorize', 'ManualAccountHandling.json')
 ParseSettings_file = _import_os.path.join('Parsing', 'ParseSettings.csv')
 Constants_file = _import_os.path.join('Root', 'Constants.py') # Not used, but good for the user to have
+BucketInfo_file = _import_os.path.join('Root', 'BucketInfo.csv')
 filepaths = [
     AutoTemplates_file,
     Templates_file,
     ManualAccountHandling_file,
     ParseSettings_file,
     Constants_file,
+    BucketInfo_file,
 ]
 
 categories = (
