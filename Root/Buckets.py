fbb4b5ac0fcbaaba921a7493b35bdeb5b8f5c2c9
Brian Glassman <15279030+BrianGlassman@users.noreply.github.com>
2023-02-24 06:13:25 -0700

    bugfix to-do category not actually being added to bucket info

--- Root/Buckets.py
+++ Root/Buckets.py
@@ -24,17 +24,19 @@ def _read_buckets() -> tuple[dict[_import_Constants.CatType, Bucket], tuple[_imp
     bucket_info = {}
     for line in lines[1:]:
         category, max_s, monthly_s = line.split(',')
+        assert category not in bucket_info
         max_f = float(max_s)
         monthly_f = float(monthly_s)
         bucket = Bucket(name=category, max_value=max_f, monthly_refill=monthly_f)
-        assert category not in bucket_info
         bucket_info[category] = bucket
     
     # Get the category names
     expense_categories = tuple(bucket_info.keys())
 
     # Add the to-do category
-    lines.append(f'{todo_category},0,0')
+    assert todo_category not in bucket_info
+    bucket = Bucket(name=todo_category, max_value=0.0, monthly_refill=0.0)
+    bucket_info[todo_category] = bucket
 
     return bucket_info, expense_categories
 bucket_info, expense_categories = _read_buckets()
