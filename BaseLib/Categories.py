97f4d5175f59b76c8b47affdc5f627dfe72401c8
Brian Glassman <>
2023-03-17 16:23:13 -0600

    don't hardcode income and internal categories

--- BaseLib/Categories.py
+++ BaseLib/Categories.py
@@ -1,35 +1,47 @@
 import csv as _import_csv
 
 import BaseLib.Constants as _import_Constants
-from BaseLib.funcs import read_commented_csv as _import_read_commented_csv
+from BaseLib.funcs import read_flagged_csv as _import_read_flagged_csv
 
 todo_category = '*** TODO ***'
+del_category = 'DELETE' # Flag to delete this transaction (keep in raw data for checksumming against sources)
 
-def _read_bucket_categories():
-    lines = _import_read_commented_csv(_import_Constants.BucketInfo_file)
-    lines = _import_csv.DictReader(lines)
-    categories = [line['Category Name'] for line in lines]
-    return categories
-# Buckets are for expenses, so just use those names
-expense_categories = tuple(_read_bucket_categories())
+def _read_bucket_names():
+    data = _import_read_flagged_csv(_import_Constants.BucketInfo_file)
+    header = data.pop('HEADER')[0]
+    fieldnames = header.split(',')
 
-# Add the to-do category
-assert todo_category not in expense_categories
+    expense_categories = None
+    income_categories = None
+    internal_categories = None
+    def _helper(categories: list[str]) -> tuple[_import_Constants.CatType]:
+        return tuple(categories)
 
-income_categories = (
-    'Parental Funds',
-    'Loans',
-    'Salary',
-    'Income - Other',
-)
+    for flag, lines in data.items():
+        lines = _import_csv.DictReader(lines, fieldnames=fieldnames)
+        categories = [line['Category Name'] for line in lines]
+        assert isinstance(categories, list)
+        assert all(isinstance(c, _import_Constants.CatType) for c in categories)
+        categories: list[str]
+        if flag == 'EXPENSES':
+            expense_categories = _helper(categories)
+        elif flag == 'INCOME':
+            income_categories = _helper(categories)
+        elif flag == 'INTERNAL':
+            internal_categories = _helper(categories)
+    
+    # Make PyLance happy
+    assert expense_categories is not None
+    assert income_categories is not None
+    assert internal_categories is not None
+    
+    return expense_categories, income_categories, internal_categories
+expense_categories, income_categories, internal_categories = _read_bucket_names()
 
-internal_categories = (
-    'CC Payments',
-    'Internal Transfers',
-)
-
+# All possible categories, including the todo and delete flags
 categories = tuple(
     list(expense_categories) + 
     list(income_categories) +
-    list(internal_categories)
+    list(internal_categories) +
+    [todo_category, del_category]
 )
--- BaseLib/funcs.py
+++ BaseLib/funcs.py
@@ -1,8 +1,35 @@
 
-def read_commented_csv(filename: str):
-    """Reads a CSV with comments (lines starting with #) and removes the comments"""
+def read_commented_csv(filename: str) -> list[str]:
+    """Reads a CSV with comments (lines starting with #) and removes the comments
+    Note: does NOT split lines"""
     with open(filename, 'r', newline='') as f:
         lines = [line.strip() for line in f.readlines()]
     lines = [line for line in lines if line] # Remove empty lines
     lines = [line for line in lines if not line.startswith('#')] # Remove comment lines
     return lines
+
+def read_flagged_csv(filename: str) -> dict[str, list[str]]:
+    """Reads a CSV with comments (lines starting with #) and flags (lines starting with $)
+    Returns a dictionary like {flag: [flagged lines]}
+    Note: does NOT split lines"""
+    lines = read_commented_csv(filename)
+
+    flag = 'HEADER'
+    out = {}
+    for line in lines:
+        if line.startswith('$'):
+            flag = line.replace('$', '').strip()
+        else:
+            if flag not in out:
+                out[flag] = []
+            out[flag].append(line)
+    
+    # Header should be one line, or removed
+    if len(out['HEADER']) == 0:
+        out.pop('HEADER')
+    elif len(out['HEADER']) == 1:
+        pass
+    else:
+        raise RuntimeError("Multiple header lines found")
+    
+    return out
