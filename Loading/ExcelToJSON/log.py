b35039f79ffe950781bbbcf64e4a72d260bdf0e1
Brian Glassman <15279030+BrianGlassman@users.noreply.github.com>
2024-08-18 11:36:14 -0600

    move is_json_stale definition

--- Loading/ExcelToJSON/log.py
+++ Loading/ExcelToJSON/log.py
@@ -102,7 +102,7 @@ def save_to_file(tag: str, contents, outfile):
 # Call it this instead of "main" to make imports easier
 def xls_to_json():
     from Loading import years
-    from Loading import is_json_stale
+    from Loading.ExcelToJSON import is_json_stale
     for year in years:
         # Check both to avoid confusing printouts
         data_stale = is_json_stale(
